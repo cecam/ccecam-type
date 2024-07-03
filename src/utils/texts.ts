88ff8e7bd2d6bccfcc4972d8bb30865680b4314f
Celso Cardenas <cardenascelso9@gmail.com>
2024-07-03 10:31:13 -0600

    add basic texts to test the app

---
+++ src/utils/texts.ts
@@ -0,0 +1,4 @@
+export const texts = [
+  "Hello, this is one of the texts that are going to be used to practice your typing skills.",
+  "You can use this text to improve your typing speed and accuracy.",
+];
