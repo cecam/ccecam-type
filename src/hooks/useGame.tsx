fad621c169949a92d8b5ddfa3ecc1087d1940095
Celso Cardenas <cardenascelso9@gmail.com>
2024-07-03 10:45:26 -0600

    functionality of start play with timer

--- src/hooks/useGame.tsx
+++ src/hooks/useGame.tsx
@@ -6,17 +6,31 @@ const MAX_TIME = 30;
 export const useGame = () => {
   const [time, setTime] = useState(0);
   const [text, setText] = useState("");
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const startGame = () => {
+    setIsPlaying(true);
     setTime(MAX_TIME);
     setText(texts[Math.floor(Math.random() * texts.length)]);
   };
 
   useEffect(() => {
+    if (!isPlaying) {
+      return;
+    }
+    if (isPlaying && time === 0) {
+      setIsPlaying(false);
+      setText(texts[Math.floor(Math.random() * texts.length)]);
+    }
+
     const interval = setInterval(() => {
       if (time > 0) {
         setTime(time - 1);
       }
     }, 1000);
-  }, []);
+
+    return () => clearInterval(interval);
+  }, [isPlaying, time]);
+
+  return { time, text, isPlaying, startGame };
 };
