8395b45179c9d59ec54660f249942ea4cf473186
Celso Cardenas <cardenascelso9@gmail.com>
2024-07-04 18:56:59 -0600

    timer implementation on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+"use client";
 import { Timer } from "../components/Timer";
 import { Header } from "../components/Header";
+import { useGame } from "@/hooks/useGame";
+
 export default function Home() {
+  const { time, isPlaying, startGame } = useGame();
   return (
     <div>
       <Header />
-      <div className="px-10 py-5">
-        <Timer time={10} />
-      </div>
+      {isPlaying ? (
+        <div className="px-10 py-5">
+          <Timer time={time} />
+        </div>
+      ) : (
+        <div className="px-10 py-5">
+          <button onClick={startGame}>Start Game</button>
+        </div>
+      )}
     </div>
   );
 }
